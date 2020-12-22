6451061e01f5d180f1dd336b7a530acfa1d75029
Thảo Mẫn <thao.manminh@gameloft.com>
2020-12-22 18:24:16 +0700

    commit

--- public/default.js
+++ public/default.js
@@ -34,7 +34,11 @@ var initGame = function(){
 	var cfg =  {
 		draggable: true,
 		position: 'start',
-		onDrop: handleMove,
+		onDrop: onDrop,
+		onDragStart: onDragStart,
+		onMouseoutSquare: onMouseoutSquare,
+		onMouseoverSquare: onMouseoverSquare,
+		onSnapEnd: onSnapEnd
 	}
 
 	board = new ChessBoard('gameBoard', cfg);
@@ -57,3 +61,93 @@ socket.on('move', function(msg){
 socket.on('history', function(data){
 	$("#listHistory").append("<div class='htr'>"+data.from+" "+data.to+"</div>");
 });
+
+//hint
+var whiteSquareGrey = '#a9a9a9'
+var blackSquareGrey = '#696969'
+
+function removeGreySquares () {
+	$('#gameBoard .square-55d63').css('background', '')
+}
+
+function greySquare (square) {
+	var $square = $('#gameBoard .square-' + square)
+  
+	var background = whiteSquareGrey
+	if ($square.hasClass('black-3c85d')) {
+	  background = blackSquareGrey
+	}
+  
+	$square.css('background', background)
+}
+
+function onDragStart (source, piece) {
+	// do not pick up pieces if the game is over
+	if (game.game_over()) return false
+  
+	// or if it's not that side's turn
+	if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+		(game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+	  return false
+	}
+}
+  
+function onDrop (source, target, piece, newPos, oldPos) {
+	removeGreySquares()
+  
+	// see if the move is legal
+	var move = game.move({
+	  from: source,
+	  to: target,
+	  promotion: 'q' // NOTE: always promote to a queen for example simplicity
+	})
+  
+	// illegal move
+	if (move === null) return 'snapback'
+
+	//history
+	console.log('Piece: ' + piece)
+	console.log('New position: ' + target)
+	console.log('Old position: ' + source)
+	if (game.turn() === 'w')
+	{
+		document.getElementById("listHistory").innerHTML += "<ol id = \"blackMove\">" + piece + ": " + source + " - " + target +"</ol>"
+		document.getElementById("nguoiChoi2").innerHTML = "Your Turn"
+		document.getElementById("nguoiChoi1").innerHTML = ""
+	}
+	else
+	{
+		document.getElementById("listHistory").innerHTML += "<ol id = \"whiteMove\">" + piece + ": " + source + " - " + target +"</ol>"
+		document.getElementById("nguoiChoi1").innerHTML = "Your Turn"
+		document.getElementById("nguoiChoi2").innerHTML = ""
+	}
+
+}
+  
+function onMouseoverSquare (square, piece) {
+	// get list of possible moves for this square
+	var moves = game.moves({
+	  square: square,
+	  verbose: true
+	})
+  
+	// exit if there are no moves available for this square
+	if (moves.length === 0) return
+  
+	// highlight the square they moused over
+	greySquare(square)
+  
+	// highlight the possible squares for this piece
+	for (var i = 0; i < moves.length; i++) {
+	  greySquare(moves[i].to)
+	}
+}
+  
+function onMouseoutSquare (square, piece) {
+	removeGreySquares()
+}
+  
+function onSnapEnd () {
+	board.position(game.fen())
+}
+
